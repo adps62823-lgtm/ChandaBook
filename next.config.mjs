898503abdc12fec93cbec2d75fd11ea7c7577f39
unknown <adps62823@gmail.com>
2026-09-13 14:27:27 +0530

    fix(vercel): add vercel.json rewrites and fallback routing to fix reload 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return {
+      beforeFiles: [],
+      afterFiles: [],
+      fallback: [
+        {
+          source: '/:path*',
+          destination: '/',
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
